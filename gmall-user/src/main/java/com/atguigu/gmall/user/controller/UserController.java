4b5ac470a3594e9d94c53dc5ac330fcf3138fe2c
yangmengheng <1310570353@qq.com>
2021-02-25 22:14:28 +0800

    构建模块

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/userInfoList")
+    public ResponseEntity<List<UserInfo>> userInfoList(){
+
+
+        List<UserInfo> userInfos = userService.userInfoList();
+
+        return ResponseEntity.ok(userInfos);
+    }
+
+}
---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+
+    @Override
+    public List<UserInfo> userInfoList() {
+        return userInfoMapper.selectAll();
+    }
+}
